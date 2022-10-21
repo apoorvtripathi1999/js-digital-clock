1eaa0a4f90f93c7635103c770081dc765213b1b8
Apoorv Tripathi <46025704+apoorvtripathi1999@users.noreply.github.com>
2022-10-21 13:45:59 +0530

    Update index.js
    Script for the clock

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Your code here
+var hours = document.getElementById('h');
+var minutes = document.getElementById('m');
+var seconds = document.getElementById('s');
+var day = document.getElementById('da');
+var date = document.getElementById('d');
+var dura = document.getElementById('ampm');
+
+function reload() {
+    var cont = new Date();
+
+    var hh = cont.getHours();
+    var mm = cont.getMinutes();
+    var ss = cont.getSeconds();
+    var dd = cont.getUTCDate();
+    var day_local = cont.getDay();
+    var duration;
+    
+    if(hh==00){
+     hh = 12;
+     duration = 'AM';
+    }
+    if(hh==12){
+     duration = 'PM';
+    }
+    if(hh>12){
+     hh = hh-12;
+     duration = 'PM';
+    }
+    if(hh<12)
+    {
+     duration = 'AM';
+    }
+
+    hours.textContent = hh; 
+    minutes.textContent = mm;
+    seconds.textContent = ss;
+    date.textContent = dd;
+    dura.textContent = duration;
+    day.textContent = getday(day_local);
+}
+
+function getday(value){
+    var dayNames =
+    [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+    return dayNames[value]
+}
+
+setInterval(reload,1000);
